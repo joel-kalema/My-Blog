7a7d78be2edaade90b4a63e44db8f539d82ca113
joel <joelkalema63@gmail.com>
2022-07-15 16:51:52 +0200

    🎉 add models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 end
--- db/migrate/20220713212506_add_users_id_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddUsersIdToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :users_id, :integer
-    add_index :posts, :users_id
-  end
-end
