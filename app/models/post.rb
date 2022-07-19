188162d661d35cb12ea09f67062cc18c120be882
joel <joelkalema63@gmail.com>
2022-07-19 17:52:47 +0200

    add router

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Posts < ApplicationRecord
+class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
