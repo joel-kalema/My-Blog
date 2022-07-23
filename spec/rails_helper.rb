1019ac588c159f7bab31b90eee55aa7b29055702
joel <joelkalema63@gmail.com>
2022-07-23 16:52:25 +0200

    fix linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install' # rubocop:todo Layout/EndOfLine
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
