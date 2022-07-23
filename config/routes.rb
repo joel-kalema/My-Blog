3157e4260f539c8b4d9c8da9b6c7b9ee9943818d
joel <joelkalema63@gmail.com>
2022-07-23 19:15:24 +0200

    fixed linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'likes/new'
   root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
     end
   end
 end
