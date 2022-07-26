48203437638ea189b29bb71612558e0ea2e03bd6
joel <joelkalema63@gmail.com>
2022-07-26 18:11:37 +0200

    Add test

---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+# Test the users index page using integration tests
+
+RSpec.describe 'Users index page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Damola', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+    @user2 = User.create(name: 'Joel kalema', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+  end
+  it 'displays all users username' do
+    visit '/'
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user2.name)
+  end
+
+  it 'displays all users profile photo' do
+    visit '/'
+    find('img[alt="Damola"]')
+    find('img[alt="Joel kalema"]')
+  end
+
+  it 'displays number of posts' do
+    visit '/'
+    expect(page).to have_content('Number of posts: 0')
+  end
+
+  it 'after clicking on a user, the user profile page is displayed' do
+    visit '/'
+    click_link @user.name
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user.bio)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+require 'support/database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -36,7 +38,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
